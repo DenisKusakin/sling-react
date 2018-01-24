7c23323cab835f3c0affa262fdecb921fcb0cdae
cathcart93 <korzhemanova12>
2018-01-24 09:55:35 +0300

    update js

--- sling-react-js/src/main/js/spectacle/index.js
+++ sling-react-js/src/main/js/spectacle/index.js
@@ -1,11 +1,19 @@
-import Deck from './deck'
-import Heading from './heading'
-import Text from './text'
-import Client from './Client'
+import dynamic from 'next/dynamic'
+
+const DynamicDeck = dynamic(import('./deck'), {
+    ssr: false
+});
+
+const DynamicHeading = dynamic(import('./heading'), {
+    ssr: false
+});
+
+const DynamicText = dynamic(import('./text'), {
+    ssr: false
+});
 
 export default {
-    Deck,
-    Heading,
-    Text,
-    Client
+    Deck: DynamicDeck,
+    Heading: DynamicHeading,
+    Text: DynamicText
 }
