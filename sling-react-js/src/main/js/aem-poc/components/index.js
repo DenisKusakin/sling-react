32ed3b3f1656eedbfe42d6ab2bc4e5eef6ce6576
dkorsakov <dimawork.spb@gmail.com>
2018-08-21 22:40:07 +0300

    narrow banner added

--- sling-react-js/src/main/js/aem-poc/components/index.js
+++ sling-react-js/src/main/js/aem-poc/components/index.js
@@ -2,10 +2,13 @@ import Heading from './heading';
 import Parsys from './parsys';
 import ImageGallery from './imageGallery/imageGallery';
 import Accordeon from './accordeon/accordeon';
+import NarrowBanner from './narrowBanner/narrowBanner';
+import './main.css';
 
 export default {
     Heading,
     Parsys,
     ImageGallery,
+    NarrowBanner,
     Accordeon
 }
